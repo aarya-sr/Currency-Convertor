3468f31684c2e203e8a710fe466e6b18ee318282
Aarya Srivastava <aaryasrivastava@Aaryas-MacBook-Air.local>
2026-04-10 16:27:44 +0530

    some functionality added

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
 console.log("File is working")
 
-URL="https://api.currencyapi.com/v3/latest?apikey=cur_live_R8rQyJUrjNKeSzMXhNG5KKxiGivbhc52UNNJolRO"
+const populate=async (value,currency)=>{
+URL="https://api.currencyapi.com/v3/latest?apikey=cur_live_R8rQyJUrjNKeSzMXhNG5KKxiGivbhc52UNNJolRO&base_currency="+currency
+let response = await fetch(URL)
+let rJson = await response.json()
+console.log(rJson)
+for(let key in Object.keys(rJson["data"])){
+
+}
+const tableBody = document.querySelector("tbody");
+tableBody.innerHTML = `
+    <tr>
+      <td>Data1</td>
+      <td>Data2</td>
+      <td>Data2</td>
+    </tr>
+    `
+}
+
+const btn = document.querySelector(".btn")
+
+btn.addEventListener("click",(e)=>{
+    e.preventDefault()
+    console.log("button is clicked")
+    const value = parseInt(document.querySelector("input[name='quantity']").value)
+    const currency = document.querySelector("input[name='quantity']").value
+    populate(value,currency)
+})
+
+const tableBody = document.querySelector("tbody");
+tableBody.innerHTML = `
+    <tr>
+      <td>Data1</td>
+      <td>Data2</td>
+      <td>Data2</td>
+    </tr>
+    `
